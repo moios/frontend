3599920a7953461948d59aa0f39f081dbde81ce0
Moisés AT <osmarmoises@gmail.com>
2026-06-02 16:29:06 -0300

    listas en el ejercicio 1 y capas en el ejercicio 2

---
+++ dia1/practica1/script.js
@@ -0,0 +1,8 @@
+
+function cambiarFondo(){
+    alert("Cambiando fondo");
+}
+
+
+document.getElementById("img1").addEventListener("click",cambiarFondo);
+document.getElementById("img2").addEventListener("click",cambiarFondo);
